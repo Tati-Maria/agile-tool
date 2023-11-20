a72118f3a7fb09794465b35131458f0e067f5b58
Maria Tati <mauramaria92@gmail.com>
2023-11-20 22:12:25 +0000

    fix: fix footer and header

--- client/src/components/home/footer.tsx
+++ client/src/components/home/footer.tsx
@@ -1,9 +1,43 @@
-
+import {FacebookIcon, TwitterIcon, LinkedinIcon, InstagramIcon} from "lucide-react"
 
 const Footer = () => {
   return (
-    <footer>Footer</footer>
-  )
+    <footer className="w-full bg-slate-900 text-white py-4">
+      <div className="container mx-auto px-4 flex flex-col items-center justify-center space-y-4">
+        <div className="flex space-x-6">
+          <a
+            aria-label="Facebook"
+            className="text-gray-300 hover:text-white"
+            href="#"
+          >
+            <FacebookIcon className="h-6 w-6" />
+          </a>
+          <a
+            aria-label="Twitter"
+            className="text-gray-300 hover:text-white"
+            href="#"
+          >
+            <TwitterIcon className="h-6 w-6" />
+          </a>
+          <a
+            aria-label="Instagram"
+            className="text-gray-300 hover:text-white"
+            href="#"
+          >
+            <InstagramIcon className="h-6 w-6" />
+          </a>
+          <a
+            aria-label="LinkedIn"
+            className="text-gray-300 hover:text-white"
+            href="#"
+          >
+            <LinkedinIcon className="h-6 w-6" />
+          </a>
+        </div>
+        <div className="text-sm text-gray-300">© All rights reserved.</div>
+      </div>
+    </footer>
+  );
 }
 
 export default Footer
--- client/src/components/home/header.tsx
+++ client/src/components/home/header.tsx
@@ -1,25 +1,36 @@
 import { Heading, Typography } from '@/components/shared';
-import { ArrowRightIcon } from 'lucide-react';
 import { Link } from 'react-router-dom';
 
 const Header = () => {
   return (
-    <header className="max-w-7xl mx-auto px-4 grid grid-cols- h-screen md:grid-cols-2">
-      <div className='flex-col-center relative w-full space-y-10 justify-center items-center text-left pl-6'>
-        <Heading className='text-5xl leading-snug md:text-7xl font-semibold w-full' level={1}>Manage Your Big Project.</Heading>
-        <Typography className='mr-auto text-base max-w-md'>
-          Worktec is a project management tool that helps you organize projects
-          and tasks in minutes.
-        </Typography>
-        <Link className='block w-full' to="/sign-up">
-          <span>Get Started</span>
-            <ArrowRightIcon className='inline-block bg-iconSecondary rounded-full ml-2 hover:animate-pulse' size={24} />
-        </Link>
+    <section className="w-full flex-center flex-col space-y-10 py-12 md:py-24 lg:py-32 xl:py-48">
+      <div className="container px-4 md:px-6">
+        <div className="flex flex-col items-center space-y-4 text-center">
+          <div className="space-y-4">
+            <Heading level={1} className="text-3xl max-w-sm md:max-w-3xl mx-auto font-semibold tracking-tighter sm:text-4xl md:text-5xl lg:text-7xl">
+              Manage Your Projects Efficiently
+            </Heading>
+            <Typography className="mx-auto font-light text-base md:text-xl max-w-[700px]">
+              Streamline your workflow and boost productivity with our intuitive
+              project management tool.
+            </Typography>
+          </div>
+          <div className="space-x-4">
+            <Link
+              className="inline-flex h-9 items-center justify-center rounded-md bg-white px-4 py-2 text-sm font-medium text-zinc-900 shadow transition-colors hover:bg-white/90 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-zinc-950 disabled:pointer-events-none disabled:opacity-50 dark:bg-zinc-50 dark:text-zinc-900 dark:hover:bg-zinc-50/90 dark:focus-visible:ring-zinc-300"
+              to="/sign-up"
+            >
+              Get Started Today
+            </Link>
+          </div>
+        </div>
       </div>
-      <div className='hidden md:flex-col-center justify-center h-full'>
-        <img className="h-96" src="/images/hero.svg" alt="team" />
-      </div>
-    </header>
+      <figure className='flex-col-center md:hidden items-center justify-center w-full'>
+        <img 
+        className='w-[80%] sm:w-[80%] lg:w-[40%]'
+         src="/images/hero.svg" alt="hero" />
+      </figure>
+    </section>
   );
 };
 
